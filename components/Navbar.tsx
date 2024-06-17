d804a544882a405f4a0b0031ac89a51832d20d7b
gabPinedaP04 <114187407+gabPinedaP04@users.noreply.github.com>
2024-06-17 11:43:14 -0600

    primer commit

---
+++ components/Navbar.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function Navbar() {
+  return (
+    <View className="w-full bg-white px-6 py-4 sm:py-6 lg:px-8">
+      <View className="bg-white border-gray-200 dark:bg-gray-900">
+        <View className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+          <Text className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+            INE 2
+          </Text>
+          <View className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
+            <Text className="sr-only">Open main menu</Text>
+          </View>
+          <View className="hidden w-full md:block md:w-auto">
+            <View className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+              <Text className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
+                Home
+              </Text>
+              <Text className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
+                About
+              </Text>
+              <Text className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
+                Services
+              </Text>
+              <Text className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
+                Pricing
+              </Text>
+              <Text className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
+                Contact
+              </Text>
+            </View>
+          </View>
+        </View>
+      </View>
+    </View>
+  );
+}
